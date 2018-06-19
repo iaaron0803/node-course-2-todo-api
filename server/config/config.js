31a1b8a34316b03b320ad683c6d1f683854a6684
iaaron0803 <iaaron0803@gmail.com>
2018-06-19 02:16:54 +0000

    Update config to use json file

--- server/config/config.js
+++ server/config/config.js
@@ -1,9 +1,18 @@
 var env = process.env.NODE_ENV || 'development';
 
-if(env === 'development'){
-    process.env.PORT = 8080;
-    process.env.MONGODB_URI = 'mongodb://user:aaron7662@ds161574.mlab.com:61574/node-db';
-}else if(env === 'test'){
-    process.env.PORT = 8080;
-    process.env.MONGODB_URI = 'mongodb://user:aaron7662@ds159100.mlab.com:59100/node-db-test';
+if(env === 'development' || env === 'test'){
+    var config = require('./config.json');
+    var envConfig = config[env];
+    
+    Object.keys(envConfig).forEach((key)=>{
+        process.env[key] = envConfig[key];
+    });
 }
+
+// if(env === 'development'){
+//     process.env.PORT = 8080;
+//     process.env.MONGODB_URI = 'mongodb://user:aaron7662@ds161574.mlab.com:61574/node-db';
+// }else if(env === 'test'){
+//     process.env.PORT = 8080;
+//     process.env.MONGODB_URI = 'mongodb://user:aaron7662@ds159100.mlab.com:59100/node-db-test';
+// }
